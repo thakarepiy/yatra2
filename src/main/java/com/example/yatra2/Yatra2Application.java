3dd35267d14bf0adb56537e9c458e32d67773444
thakarepiy <thakarepiyush@gmail.com>
2023-02-26 17:42:02 +0530

    CodePush

---
+++ src/main/java/com/example/yatra2/Yatra2Application.java
@@ -0,0 +1,13 @@
+package com.example.yatra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yatra2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yatra2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yatra2/Yatra2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatra2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yatra2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

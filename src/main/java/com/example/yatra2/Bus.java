79848ce9b76190ab6a4027a4734f61ec4b6e3706
thakarepiy <thakarepiyush@gmail.com>
2023-02-26 17:56:32 +0530

    NewClass

---
+++ src/main/java/com/example/yatra2/Bus.java
@@ -0,0 +1,14 @@
+package com.example.yatra2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Bus {
+    @GetMapping("/mybus")
+    public String getData(){
+        return "Please book your bus tickets, Aaliya Express Lines";
+    }
+}
